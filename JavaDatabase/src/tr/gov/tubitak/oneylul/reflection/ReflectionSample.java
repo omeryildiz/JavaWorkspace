677a12d3356b5cb7b64d492f3c0b71c03e9fa123
Omer YILDIZ <nomeryildiz@gmail.com>
2015-09-10 11:43:27 +0300

    başka bir paketteki sınıftan bir nesne oluşturduktan sonra nesnenin değerini değiştirebiliyoruz.

--- JavaDatabase/src/tr/gov/tubitak/oneylul/reflection/ReflectionSample.java
+++ JavaDatabase/src/tr/gov/tubitak/oneylul/reflection/ReflectionSample.java
@@ -1,20 +1,14 @@
 package tr.gov.tubitak.oneylul.reflection;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
-
-
-
 public class ReflectionSample {
-	public static void main(String[] args) {
-		Class clazz = null;
-		try {
-			clazz = Class.forName("tr.gov.tubitak.oneylul.Ogrenci");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public static void main(String[] args) throws Exception {
+
+		Class clazz = Class.forName("tr.gov.tubitak.oneylul.Ogrenci");
+
 		System.out.println(clazz.getSimpleName());
 		System.out.println(clazz.getName());
 		Method[] methods = clazz.getMethods();
@@ -25,19 +19,27 @@ public class ReflectionSample {
 		for (Field field2 : fields) {
 			System.out.println(field2.getName());
 		}
-		//baþka bir paketteki bir sýnýfýn private alanlarýnýn listelenmesi
-		try {
-			//Field idField = clazz.getDeclaredField("id");
-			Field[] idField = clazz.getDeclaredFields();
-			for (Field field : idField) {
-				System.out.println(field);
-			}
-			//idField.setAccessible(true);
-			//System.out.println(idField);
-		} catch (Exception e) {
-			e.printStackTrace();
+		// baþka bir paketteki bir sýnýfýn private alanlarýnýn listelenmesi
+
+		// Field idField = clazz.getDeclaredField("id");
+		Field[] idField = clazz.getDeclaredFields();
+		for (Field field : idField) {
+			System.out.println(field);
 		}
+		// idField.setAccessible(true);
+		// System.out.println(idField);
+
+		Constructor constructor = clazz.getConstructor();
+		Object obj = constructor.newInstance();
+		Method setIdMethod = clazz.getMethod("setAd", String.class);
+		setIdMethod.invoke(obj, "omer");
+		System.out.println(obj);
 		
+		Method getAdMethod = clazz.getMethod("getAd");
+	 	System.out.println(getAdMethod.invoke(obj));
+	 	System.out.println(obj);
+		
+
 	}
-	
+
 }
