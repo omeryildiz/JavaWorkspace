7cbb01348473adb71605b9201e3a67911fd6ab05
Omer YILDIZ <nomeryildiz@gmail.com>
2015-09-16 01:43:10 +0300

    başka bir eclipse üzerinde açmanın neden olduğu commitler

---
+++ home-work-sample/target/classes/tr/gov/tubitak/onbeseylul/Employee.java
@@ -0,0 +1,17 @@
+package tr.gov.tubitak.onbeseylul
+public class Employee {
+    private String employeeId;
+    private String employeeName;
+    public String getEmployeeId() {
+        return employeeId;
+    }
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+    public String getEmployeeName() {
+        return employeeName;
+    }
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+}
---
+++ home-work-sample/target/home-work-sample/WEB-INF/classes/Employee.java
@@ -0,0 +1,7 @@
+
+public class Employee {
+	private String id;
+	private String name;
+	private Float grossSalary;
+
+}
