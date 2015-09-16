8b8dd9632dadd8e6400ac3ffd90b0844d5ce5067
Omer YILDIZ <nomeryildiz@gmail.com>
2015-09-16 16:29:19 +0300

    eğitimde kullanılan sınıflar eklendi

---
+++ pf-sample-omeryildiz/src/main/java/org/omeryildiz/pfsample/Student.java
@@ -0,0 +1,36 @@
+package org.omeryildiz.pfsample;
+
+import java.util.Date;
+
+public class Student {
+	private Integer id;
+	private String name;
+	private String surname;
+	private Date date;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+
+}
---
+++ pf-sample-omeryildiz/src/main/java/org/omeryildiz/pfsample/StudentBean.java
@@ -0,0 +1,56 @@
+package org.omeryildiz.pfsample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class StudentBean {
+	private List<Student> students = new ArrayList<Student>();
+	private Student student = new Student();
+
+//	public void init() { @postconstruct annotation'ý çalýþmýyor ayrý bir dependency eklemek gerekiyor olmalý
+//		Student stud = new Student();
+//		stud.setId(1);
+//		stud.setName("Ahmet");
+//		stud.setSurname("YILDIZ");
+//		this.students.add(stud);
+//	}
+	
+	public void select(Student student) {
+		this.student = student;
+	}
+
+	public void create() {
+		this.students.add(getStudent());
+		this.student = new Student();
+	}
+
+	public void delete(Student student) {
+		students.remove(student);
+	}
+
+	public void update() {
+		this.student = new Student();
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+}
